9ac9c5f5852d8988b7e22c51eb0bca6fd2d15f22
Ndet0 <festusndet00@gmail.com>
2025-11-29 16:15:05 +0300

    initial commit

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self, items=None):
+        self.dictionary = {}
+        if items:
+            for item in items:
+                self.dictionary[item] = True
 
-    pass
+    def add(self, item):
+        self.dictionary[item] = True
+
+    def delete(self, item):
+        if item in self.dictionary:
+            del self.dictionary[item]
+
+    def has(self, item):
+        return item in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
